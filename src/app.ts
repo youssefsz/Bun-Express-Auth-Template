095087edba970dafb42781eca7f3a76c35521f3e
youssefsz <darhoumyi@gmail.com>
2026-01-27 22:34:46 +0100

    feat: add morgan request logger to server
    - Install morgan and @types/morgan
    - Configure morgan dev logging middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import authRoutes from './routes/auth.routes';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
